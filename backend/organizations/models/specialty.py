d367f85f24b34331ca22b055fabc8a27a1769ec2
rofel <vgolosyandriya@yandex.ru>
2023-12-03 10:07:23 +0300

    added specialty model

--- backend/organizations/models/specialty.py
+++ backend/organizations/models/specialty.py
@@ -2,13 +2,11 @@ from django.db import models
 
 
 class Specialty(models.Model):
-    """Описание модели специальности врача.
-    # https://base.garant.ru/70480868/7dede6ac8f25be619ed07c17ed1c62c9/
-    """
+    """Описание модели специальности врача."""
 
-    code = models.CharField(max_length=8, primary_key=True)
-    name = models.CharField(max_length=150)
-    skill = models.CharField(max_length=150)
+    code = models.CharField('Код', max_length=8, primary_key=True)
+    name = models.CharField('Наименование', max_length=150)
+    skill = models.CharField('Врач', max_length=150)
 
     class Meta:
         ordering = ['code']
