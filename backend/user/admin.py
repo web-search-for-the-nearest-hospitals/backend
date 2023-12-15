80f9b4a2509715bee0130c7294c7d2d5674a77be
Roman Yegorov <e.roman.v@ya.ru>
2023-12-16 01:51:26 +0300

    fixed pep8 #13

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -2,22 +2,20 @@ from django.contrib import admin
 
 from user.models import User
 
+
 class BaseAdmin(admin.ModelAdmin):
-    empty_value_display = '-пусто-'
+    empty_value_display = "-пусто-"
+
 
 @admin.register(User)
 class UserAdmin(BaseAdmin):
     list_display = (
-        'username',
-        'email',
-        'role',
-        'first_name',
-        'last_name',
-        'phone',
+        "username",
+        "email",
+        "role",
+        "first_name",
+        "last_name",
+        "phone",
     )
-    search_fields = (
-        'username',
-        'role',
-        'phone'
-    )
-    list_filter = ('username',)
+    search_fields = ("username", "role", "phone")
+    list_filter = ("username",)
--- backend/user/models.py
+++ backend/user/models.py
@@ -2,46 +2,49 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     """
     Модель пользователя с дополнительной информацией
     """
-    class Roles(models.TextChoices):
-        ADMIN = 'admin'
-        DOCTOR = 'doctor'
-        USER = 'user'
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('username',)
-    
-    email = models.EmailField(db_index=True,
+    class Roles(models.TextChoices):
+        ADMIN = "admin"
+        DOCTOR = "doctor"
+        USER = "user"
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ("username",)
+
+    email = models.EmailField(
+        db_index=True,
         unique=True,
         max_length=254,
-        help_text='Укажите email пользователя')
+        help_text="Укажите email пользователя",
+    )
     role = models.CharField(
         max_length=9,
         choices=Roles.choices,
         default=Roles.USER,
-        verbose_name='роль',
-        help_text='Выберите роль (администратор/доктор/пациент)',
+        verbose_name="роль",
+        help_text="Выберите роль (администратор/доктор/пациент)",
     )
     first_name = models.CharField(
-        verbose_name='имя',
+        verbose_name="имя",
         max_length=150,
         null=True,
     )
     last_name = models.CharField(
-        verbose_name='фамилия',
+        verbose_name="фамилия",
         max_length=150,
         null=True,
     )
     phone = models.CharField(max_length=15, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
- 
 
     def __str__(self):
         return self.email
-    
+
     @property
     def is_admin(self):
         return self.is_staff or self.role == self.Roles.ADMIN
@@ -49,4 +52,3 @@ class User(AbstractUser):
     @property
     def is_doctor(self):
         return self.role == self.Roles.DOCTOR
-
