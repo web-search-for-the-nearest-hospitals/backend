4995ec9d5e0e3d4ae76b81221236c2ad2b00bdc3
Магомед Юнусов <deathrow95@mail.ru>
2023-12-25 22:26:58 +0300

    Тесты для API организаций

--- tests/fixtures/test_data.py
+++ tests/fixtures/test_data.py
@@ -1,39 +1,52 @@
 import pytest
 from backend.organizations.models import Organization, Town, District
+from django.urls import reverse
+from uuid import uuid4
 
 
 @pytest.fixture
-def Just_town():
+def just_town():
     return Town.objects.create(
-        name='Just Town',
+        name='just Town',
         longitude=0.0,
         latitude=0.0
     )
 
 
 @pytest.fixture
-def Just_district(Just_town):
+def just_district(just_town):
     return District.objects.create(
-        name='Just District',
-        town=Just_town
+        name='just District',
+        town=just_town
     )
 
 
 @pytest.fixture
-def Just_organization(Just_town, Just_district):
+def just_organization(just_town, just_district):
     return Organization.objects.create(
-        full_name='Just Organization',
-        short_name='Just Org',
+        full_name='just Organization',
+        short_name='just Org',
         inn='1234567890',
-        factual_address='Just Address',
+        factual_address='just Address',
         longitude=0.0,
         latitude=0.0,
-        site='http://Just.ru',
-        email='Just@gmail.com',
+        site='http://just.org',
+        email='just@gmail.com',
         phone='123-456-789',
         is_gov=False,
         is_full_time=True,
-        about='Just description',
-        town=Just_town,
-        district=Just_district
+        about='just description',
+        town=just_town,
+        district=just_district
     )
+
+
+@pytest.fixture
+def api_url():
+    return '/api/organizations'
+
+
+@pytest.fixture
+def api_url_with_uuid(just_organization):
+    organization_uuid = str(uuid4())
+    return reverse('organization-detail', kwargs={'uuid': organization_uuid})
--- tests/test_api_organizations.py
+++ tests/test_api_organizations.py
@@ -1,19 +1,14 @@
 import pytest
-from django.urls import reverse
+from fixtures.test_data import (api_url, api_url_with_uuid, just_organization)
 
-
-API_BASE_URL = 'http://localhost:8000/api/organizations'
-
-
-@pytest.fixture
-def api_url():
-    return f'{API_BASE_URL}/organizations'
+API_BASE_URL = 'http://localhost:8000/api'
 
 
 @pytest.mark.django_db
-def test_get_organizations(api_client):
-    response = api_client.get('/api/organizations')
-    assert response.status_code == 200
+def test_get_organizations(api_client, just_organization):
+    response = api_client.get(api_url)
+    assert response.status_code == 200, (
+        f'Ошибка запроса: {response.status_code}')
 
     data = response.json()
     assert all(key in data for key in ['count', 'next', 'previous', 'results'])
@@ -29,12 +24,10 @@ def test_get_organizations(api_client):
 
 
 @pytest.mark.django_db
-def test_get_organization_by_uuid(api_client, sample_organization):
-    organization_uuid = sample_organization.uuid
-    url = reverse('organization-detail', kwargs={'uuid': organization_uuid})
-
-    response = api_client.get(url)
-    assert response.status_code == 200
+def test_get_organization_by_uuid(api_client, just_organization):
+    response = api_client.get(api_url_with_uuid)
+    assert response.status_code == 200, (
+        f'Ошибка запроса: {response.status_code}')
 
     expected_keys = ['full_name', 'short_name', 'inn',
                      'factual_address', 'longitude', 'latitude',
