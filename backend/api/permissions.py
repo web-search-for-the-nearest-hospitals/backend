aec32cfce9b352ad63366feea758baee550382ed
PrudyvusP <vgolosyandriya@yandex.ru>
2024-01-19 14:41:24 +0300

    fixed pep-8 round 2

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,6 +3,5 @@ from rest_framework import permissions
 
 class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS or
-                request.user.is_superuser or obj.owner == request.user
-                )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_superuser or obj.owner == request.user)
