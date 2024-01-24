144e9b592b69478d1d43c5cc55a00d26965e1667
PavlovMaximYurevich <118805571+PavlovMaximYurevich@users.noreply.github.com>
2024-01-24 21:36:00 +0300

    оформлена документация, добавлен слэш в урл джосера (#119) (#128)
    * оформлена документация, добавлен слэш в урл джосера (#119)

--- backend/api/schemas.py
+++ backend/api/schemas.py
@@ -4,6 +4,7 @@ APPOINTMENT_TAG = 'Талоны'
 MAIN_TAG = 'Организации'
 SPEC_TAG = 'Специальности врачей'
 TOWN_TAG = 'Города'
+MANAGMENT_ACCOUNT = "Управление учётной записью"
 
 RESPONSE_404 = openapi.Response('Страница не найдена.',
                                 examples={
@@ -11,6 +12,47 @@ RESPONSE_404 = openapi.Response('Страница не найдена.',
                                         "detail": "Страница не найдена."}
                                 })
 RESPONSE_204_FOR_APPOINTMENT = openapi.Response('Нет контента')
+
+RESPONSE_400_REGISTRATION = openapi.Response(
+    'Пользователь с такой почтой уже существует!',
+    examples={
+        "application/json": {
+            "detail": "Пользователь с такой почтой уже существует!"}
+    })
+
+RESPONSE_201_REGISTRATION = openapi.Response(
+    'Пользователь успешно создан!',
+    examples={
+        "application/json": {
+            "detail": "Пользователь успешно создан!"}
+    })
+
+RESPONSE_400_AUTHORIZATION = openapi.Response(
+    'Неверное имя пользователя или пароль!',
+    examples={
+        "application/json": {
+            "detail": "Неверное имя пользователя или пароль!"}
+    })
+
+RESPONSE_200_AUTHORIZATION = openapi.Response(
+    'Авторизация успешна!',
+    examples={
+        "application/json": {
+            "detail": "Авторизация успешна!",
+            "access": "access_token",
+            "refresh": "refresh_token"
+        }
+    })
+
+RESPONSES_REGISTRATION = {
+    '201': RESPONSE_201_REGISTRATION,
+    '400': RESPONSE_400_REGISTRATION
+}
+
+RESPONSES_AUTHORIZATION = {
+    '200': RESPONSE_200_AUTHORIZATION,
+    '400': RESPONSE_400_AUTHORIZATION
+}
 
 RESPONSES_WITH_404 = {
     '404': RESPONSE_404
@@ -130,5 +172,27 @@ APPOINT_SCHEMAS = {
             "summary": "Запись к врачу",
             "description": "Страница доступна всем.",
             "responses": RESPONSES_FOR_APPOINTMENT
+        }
+}
+
+SIGNUP_SCHEMAS = {
+    "post":
+        {
+            "tags": [MANAGMENT_ACCOUNT],
+            "summary": "Создание нового пользователя",
+            "description": "Используется для регистрации."
+                           " Нужно ввести емейл и пароль.",
+            "responses": RESPONSES_REGISTRATION
+        }
+}
+
+LOGIN_SCHEMAS = {
+    "post":
+        {
+            "tags": [MANAGMENT_ACCOUNT],
+            "summary": "Авторизация существующего пользователя",
+            "description": "Используется для авторизации по емейлу и паролю,"
+                           " чтобы далее использовать токен при запросах.",
+            "responses": RESPONSES_AUTHORIZATION
         }
 }
