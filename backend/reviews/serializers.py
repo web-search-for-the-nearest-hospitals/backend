d1bc17b8c27d70d15939e367830ae8bef802fa9c
PavlovMaximYurevich <118805571+PavlovMaximYurevich@users.noreply.github.com>
2024-02-01 21:58:02 +0300

    сделана документация для отзывов, добавлены миксины для вью (#139) (#143)
    * сделана документация для отзывов, добавлены миксины для вью (#139)
    
    
    ---------
    
    Co-authored-by: rofel <vgolosyandriya@yandex.ru>

--- backend/reviews/serializers.py
+++ backend/reviews/serializers.py
@@ -6,18 +6,56 @@ from organizations.models import Organization
 from reviews.models import Review
 
 
-class ReviewSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        slug_field='email',
-        read_only=True,
+class ReviewListSerializer(serializers.ModelSerializer):
+    """Сериализатор отзывов для LIST-метода."""
+
+    id = serializers.IntegerField(
+        label='id отзыва',
+        help_text='id отзыва',
+        read_only=True)
+
+    text = serializers.CharField(
+        label='Текст',
+        help_text='Текст отзыва',
+        read_only=True
     )
 
-    score = serializers.IntegerField(min_value=1, max_value=5)
+    first_name = serializers.CharField(
+        source='author.first_name',
+        help_text='Имя автора отзыва',
+        required=False)
+
+    score = serializers.IntegerField(
+        label='Оценка организации',
+        help_text='Оценка организации от 1 до 5',
+        read_only=True
+    )
+    pub_date = serializers.CharField(
+        help_text='Дата публикации отзыва',
+        read_only=True
+    )
 
     class Meta:
         model = Review
-        fields = ('id', 'text', 'author', 'score', 'pub_date')
-        read_only_fields = ('id',)
+        fields = ('id', 'text', 'first_name', 'score', 'pub_date')
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(
+        min_length=2,
+        max_length=150,
+        required=True,
+        help_text='Имя пользователя',
+        write_only='True'
+    )
+
+    score = serializers.IntegerField(
+        min_value=1, max_value=5,
+        help_text='Оценка организации')
+
+    class Meta:
+        model = Review
+        fields = ('text', 'score', 'pub_date', 'first_name')
 
     def validate(self, data):
         request = self.context.get('request')
@@ -31,3 +69,10 @@ class ReviewSerializer(serializers.ModelSerializer):
             ).exists():
                 raise serializers.ValidationError('Вы уже оставили отзыв!')
         return data
+
+    def create(self, validated_data):
+        r = self.context.get('request')
+        user = r.user
+        user.first_name = validated_data.pop('first_name')
+        user.save(update_fields=['first_name'])
+        return super().create(validated_data)
--- backend/reviews/views.py
+++ backend/reviews/views.py
@@ -1,22 +1,47 @@
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
-from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.response import Response
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
 
 from organizations.models import Organization
+from organizations.paginators import CustomNumberPagination
 from .permissions import IsAuthorOrIsAuthenticated
-from .serializers import ReviewSerializer
+from .schemas import REVIEWS_SCHEMAS
+from .serializers import ReviewSerializer, ReviewListSerializer
 
 
-class ReviewViewSet(viewsets.ModelViewSet):
+@method_decorator(
+    name="list",
+    decorator=swagger_auto_schema(
+        tags=REVIEWS_SCHEMAS["list"]["tags"],
+        operation_summary=REVIEWS_SCHEMAS["list"]["summary"],
+        operation_description=REVIEWS_SCHEMAS["list"]["description"],
+        responses=REVIEWS_SCHEMAS["list"]["responses"],
+        security=[],
+        manual_parameters=REVIEWS_SCHEMAS['list']['params'],
+    ),
+)
+@method_decorator(
+    name="create",
+    decorator=swagger_auto_schema(
+        tags=REVIEWS_SCHEMAS["create"]["tags"],
+        operation_summary=REVIEWS_SCHEMAS["create"]["summary"],
+        operation_description=REVIEWS_SCHEMAS["create"]["description"],
+        responses=REVIEWS_SCHEMAS["create"]["responses"],
+        manual_parameters=REVIEWS_SCHEMAS['create']['params']
+    ),
+)
+class ReviewViewSet(CreateModelMixin,
+                    ListModelMixin,
+                    viewsets.GenericViewSet):
     """Вьюсет для отзывов."""
-    serializer_class = ReviewSerializer
 
     permission_classes = (
         IsAuthorOrIsAuthenticated,
     )
 
-    pagination_class = LimitOffsetPagination
+    pagination_class = CustomNumberPagination
 
     def get_organization(self):
         return get_object_or_404(
@@ -32,11 +57,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
             author=self.request.user, organization=self.get_organization()
         )
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(
-            instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ReviewListSerializer
+        return ReviewSerializer
