9d0a2dc3f83579fcd6cf57115f4842f03fe8e7d6
rofel <vgolosyandriya@yandex.ru>
2024-02-15 20:16:57 +0300

    a try to use smtp 25 port wo authenticate

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,9 +129,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
+EMAIL_USE_SSL = (os.getenv('EMAIL_USE_SSL', 'False').lower()
+                 == 'true')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
